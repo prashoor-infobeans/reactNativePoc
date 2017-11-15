18d044f5c8d082b51bfd983ca5363f045cc0781d
Rohit-Infobeans <rohit.gupta@infobeans.com>
2017-11-15 19:54:20 +0530

    Update

--- src/home.js
+++ src/home.js
@@ -5,15 +5,64 @@ import DeviceStorage from 'react-device-storage';
 class Hometemplate extends React.Component {
     constructor() {
         super();
-        this.state = { };
-        //this.onSubmit = this.handleSubmit.bind(this);
+        this.state = {};
+        this.onSubmit = this.handleSubmit.bind(this);
+        this.storage = new DeviceStorage({
+            cookieFallback: true,
+            cookie: {
+                secure: true
+            }
+        }).localStorage();
+
+        this.state = {
+            isLoggedIn: this.storage.read('isLoggedIn')
+        };
     }
     handleChange(e) {
         e.target.classList.add("active");
-        
+
         this.setState({
-	      [e.target.id]: e.target.value
-	    });
+            [e.target.id]: e.target.value
+        });
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        fetch(
+            'http://rohit.corporatesitepoc.com/index.php/wp-json/dummyForm/update',
+            {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user_login: this.state.user_login,
+                    user_pass: this.state.user_pass
+                })
+            }).then(resp => {
+            let copyRes = resp.clone();
+
+            resp.json().then(rp => {
+                let response = JSON.parse(rp);
+                if (response.permission) {
+                    this.setName(response.permission);
+                    window.location.reload();
+                }
+            }).catch(err => {
+
+                copyRes.text().then(rp => {
+                    console.log(rp);
+                });
+
+            });
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+    setName(premission) {
+        this.storage.save('isLoggedIn', premission);
+        this.setState({
+          premission
+        });
     }
     render() {
         return (
@@ -23,7 +72,7 @@ class Hometemplate extends React.Component {
                             <h1>Sign In</h1>
                         </div>
                         <div className="loginFormModule">
-                            <form name="loginform" id="loginform"  method="post">
+                            <form name="loginform" id="loginform"  method="post" onSubmit={this.onSubmit}>
                                 <div className="input-text-box-wp">
                                     <div className="icon-login-user"></div>
                                     <input placeholder="abc@xyz.com" type="text" name="log" id="user_login" className="input input-mid-box width-100" value={this.state.user_login} onChange={this.handleChange.bind(this)} />
